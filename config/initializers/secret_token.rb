877ef0e2069fa7d9ff36d51824e4c279eb14c094
Casey Li <casey.li@gmail.com>
2011-07-07 20:35:15 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaseyLiResume::Application.config.secret_token = '51c51ea908670d228b09790a1537efbf8f2ee5f17a0ee2b53d4a6cbab48c0a6daa8b7506ee57a23ae9adf5977478c38944f6ac677ad29c75e2d88c074c3d71b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CaseyLiResume::Application.config.session_store :cookie_store, :key => '_casey_li_resume_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CaseyLiResume::Application.config.session_store :active_record_store
