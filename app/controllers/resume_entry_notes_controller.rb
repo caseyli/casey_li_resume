c7be07a39fbe5da400155a029899d36ec3ddf769
Casey Li <casey.li@gmail.com>
2011-08-09 21:28:36 -0400

    Fixed Bug with blank links. Also fixed JQuery issue breaking deletes.

--- app/controllers/resume_entry_notes_controller.rb
+++ app/controllers/resume_entry_notes_controller.rb
@@ -8,6 +8,14 @@ class ResumeEntryNotesController < ApplicationController
   
   def create
     @resume_entry_note = ResumeEntryNote.new(params[:resume_entry_note])
+    
+    # If no URL is entered, set values to NIL
+    if @resume_entry_note.url.blank?
+      @resume_entry_note.url = nil
+      @resume_entry_note.link_text = nil
+    end
+    
+    # Create Entry
     if @resume_entry_note.save
       redirect_to @resume_entry_note.resume_entry
     else
@@ -29,6 +37,14 @@ class ResumeEntryNotesController < ApplicationController
   
   def update
     @resume_entry_note = ResumeEntryNote.find(params[:id])
+    
+    # If no URL is entered, set values to NIL
+    if params[:resume_entry_note][:url].blank?
+      params[:resume_entry_note][:url] = nil
+      params[:resume_entry_note][:link_text] = nil
+    end
+    
+    # Update entry
     if @resume_entry_note.update_attributes(params[:resume_entry_note])
       redirect_to @resume_entry_note.resume_entry
     else
