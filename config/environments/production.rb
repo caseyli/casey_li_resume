135777fafd1c0d5ed2914357ca29b20ab3404a43
Casey Li <casey.li@gmail.com>
2011-10-27 00:55:56 -0400

    Added password to production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,14 @@ CaseyLiResume::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # GMail Configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'caseyliresume',
+    :password             => 'hirecasey',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
