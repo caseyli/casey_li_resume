dbd6fea1caa930ff29fc7d12b740d279c4c53b24
Casey Li <casey.li@gmail.com>
2014-09-24 15:44:03 -0400

    Updated framework.

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,8 @@ module CaseyLiResume
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
+    # config.i18n.enforce_available_locales = true
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
